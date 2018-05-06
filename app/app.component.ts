9722be9772596ca00c22991bc9ad4cf3cccb0127
Volodymyr Tsurko <sonicpro@ukrpost.ua>
2018-05-06 22:22:25 +0300

    Application files added. typescript package downgraded.

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+// Import the decorator class for Component
+import { Component } from
+'@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Hello, World</h1>'
+})
+export class AppComponent { }
